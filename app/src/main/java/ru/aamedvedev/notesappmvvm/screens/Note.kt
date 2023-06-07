52389327e273ef763ba2d8018854bd47ce85e663
alexey951 <leshamedvedev60@gmail.com>
2023-06-07 20:14:00 +0300

    lesson 1

---
+++ app/src/main/java/ru/aamedvedev/notesappmvvm/screens/Note.kt
@@ -0,0 +1,9 @@
+package ru.aamedvedev.notesappmvvm.screens
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun Note(navController: NavHostController) {
+
+}
---
+++ app/src/main/java/ru/aamedvedev/notesappmvvm/screens/Start.kt
@@ -0,0 +1,10 @@
+package ru.aamedvedev.notesappmvvm.screens
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun Start(navController: NavHostController) {
+    Text(text = "Note App Test")
+}
