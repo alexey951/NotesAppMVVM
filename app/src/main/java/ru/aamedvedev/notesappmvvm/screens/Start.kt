75f8b5dd9f3d66dc65bbe34d064556a463020643
alexey951 <leshamedvedev60@gmail.com>
2023-06-07 22:18:41 +0300

    Lesson-2

--- app/src/main/java/ru/aamedvedev/notesappmvvm/screens/Start.kt
+++ app/src/main/java/ru/aamedvedev/notesappmvvm/screens/Start.kt
@@ -1,10 +1,60 @@
 package ru.aamedvedev.notesappmvvm.screens
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import ru.aamedvedev.notesappmvvm.navigation.NavRoute
+import ru.aamedvedev.notesappmvvm.ui.theme.NotesAppMVVMTheme
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
-fun Start(navController: NavHostController) {
-    Text(text = "Note App Test")
+fun StartScreen(navController: NavHostController) {
+    Scaffold(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(text = "What will we use?")
+            Button(
+                onClick = {
+                    navController.navigate(route = NavRoute.Main.route)
+                },
+                modifier = Modifier
+                    .width(200.dp)
+                    .padding(vertical = 8.dp)
+            ) {
+                Text(text = "Room database")
+            }
+            Button(
+                onClick = {
+                    navController.navigate(route = NavRoute.Main.route)
+                },
+                modifier = Modifier
+                    .width(200.dp)
+                    .padding(vertical = 8.dp)
+            ) {
+                Text(text = "Firebase database")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun prevStartScreen() {
+    NotesAppMVVMTheme {
+        StartScreen(navController = rememberNavController())
+    }
 }
